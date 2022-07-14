f7de3488871502a2f640449f84c1799655063b14
VishalMakwana20 <vishal882000@gmail.com>
2022-07-15 00:32:31 +0530

    update

--- lib/bottom_navigation/category.dart
+++ lib/bottom_navigation/category.dart
@@ -1,3 +1,4 @@
+import 'package:eastern_demo/constant/const_image.dart';
 import 'package:eastern_demo/model/category_model.dart';
 import 'package:eastern_demo/widget/show_image.dart';
 import 'package:flutter/material.dart';
@@ -43,51 +44,113 @@ class _CategoryState extends State<Category> {
       // appBar: AppBar(
       //   title: Text('Category'),
       // ),
-      body: _buildBody(),
+      body: SafeArea(child: _buildBody()),
     );
   }
 
   Widget _buildBody() {
-    return StreamBuilder<List<Categories>>(
-        stream: categoryBloc!.stream,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            var list = snapshot.data!;
-            return Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                      itemCount: list.length,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        return Column(
-                          children: [
-                            const Divider(
-                              height: 1,
-                            ),
-                            Container(
-                              decoration: const BoxDecoration(
-                                  image: DecorationImage(
-                                image: NetworkImage(
-                                    "https://placeimg.com/868/430/fabric"),
-                                fit: BoxFit.cover,
-                              )),
-                              height: 150,
-                              child: SizedBox.expand(),
-                            ),
-                            const Divider(
-                              height: 1,
-                            ),
-                          ],
-                        );
-                      }),
-                )
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return Center(child: Text('${snapshot.error}'));
-          }
-          return Center(child: const CircularProgressIndicator());
-        });
+    return Column(
+      children: [
+        _buildAppBar(),
+        Expanded(
+          child: StreamBuilder<List<Categories>>(
+              stream: categoryBloc!.stream,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  var list = snapshot.data!;
+                  return Column(
+                    children: [
+                      ListView.builder(
+                          itemCount: list.length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              children: [
+                                const Divider(
+                                  height: 1,
+                                ),
+                                Container(
+                                  decoration: const BoxDecoration(
+                                      image: DecorationImage(
+                                    image: NetworkImage(
+                                        "https://placeimg.com/868/430/fabric"),
+                                    fit: BoxFit.cover,
+                                  )),
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.18,
+                                  child: SizedBox.expand(
+                                    child: Text(list[index].categoryName!),
+                                  ),
+                                ),
+                                const Divider(
+                                  height: 1,
+                                ),
+                              ],
+                            );
+                          })
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('${snapshot.error}'));
+                }
+                return const Center(child: CircularProgressIndicator());
+              }),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAppBar() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15, right: 20, left: 20, bottom: 15),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const ShowImage(
+            imagePath: ConstImage.leftBack,
+            height: 20,
+            width: 20,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          const Expanded(
+              child: Text(
+            'Category',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+            textAlign: TextAlign.center,
+          )),
+          const ShowImage(
+            imagePath: ConstImage.search,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Stack(
+            children: [
+              const ShowImage(
+                imagePath: ConstImage.shopping,
+                height: 24,
+                width: 24,
+              ),
+              Positioned(
+                  right: 0,
+                  top: 0,
+                  child: Container(
+                    width: 20,
+                    height: 20,
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.grey),
+                    child: const Text(
+                      '1',
+                      style: TextStyle(color: Colors.white, fontSize: 10),
+                    ),
+                  ))
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/constant/const_image.dart
+++ lib/constant/const_image.dart
@@ -5,4 +5,7 @@ class ConstImage {
   static const String message = _assetPath + 'message.svg';
   static const String profile = _assetPath + 'profile.svg';
   static const String setting = _assetPath + 'setting.svg';
+  static const String leftBack = _assetPath + 'left_back_arrow.svg';
+  static const String search = _assetPath + 'search.svg';
+  static const String shopping = _assetPath + 'shopping.svg';
 }
